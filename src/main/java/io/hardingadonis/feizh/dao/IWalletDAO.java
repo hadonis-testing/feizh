6b40cad7d649b2a91d859e227390c689942ed16b
Minh Vương <hardingadonis@gmail.com>
2023-12-14 15:55:54 +0700

    Add IWalletDAO and its implementer

---
+++ src/main/java/io/hardingadonis/feizh/dao/IWalletDAO.java
@@ -0,0 +1,17 @@
+package io.hardingadonis.feizh.dao;
+
+import io.hardingadonis.feizh.model.*;
+import java.util.*;
+
+public interface IWalletDAO {
+
+    public List<Wallet> getAll();
+    
+    public Wallet get(int ID);
+    
+    public void insert(Wallet obj);
+    
+    public void update(Wallet obj);
+    
+    public void delete(int ID);
+}
