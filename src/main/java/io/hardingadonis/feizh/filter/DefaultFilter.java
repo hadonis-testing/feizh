c4356010c2f618cfe47ce19ec921065899dac87f
Minh Vương <hardingadonis@gmail.com>
2023-12-18 14:54:46 +0700

    Add filter

---
+++ src/main/java/io/hardingadonis/feizh/filter/DefaultFilter.java
@@ -0,0 +1,36 @@
+package io.hardingadonis.feizh.filter;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+@WebFilter(filterName = "DefaultFilter", urlPatterns = {"/*"})
+public class DefaultFilter implements Filter {
+
+    public DefaultFilter() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String url = req.getServletPath();
+
+        if (url.equals("/") || url.contains(".jsp")) {
+            res.sendRedirect(req.getContextPath() + "/dashboard");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+}
